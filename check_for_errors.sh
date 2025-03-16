e91e2961583086e13a251a41d84f4720c31b8571
Sam King <kingst@gmail.com>
2025-03-16 12:24:47 -0700

    Add dates to scripts

--- check_for_errors.sh
+++ check_for_errors.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # Check if an argument was provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <function>"
-    echo "Example: $0 iob"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <function> YYYY-MM-DD"
+    echo "Example: $0 iob 2025-03-15"
     exit 1
 fi
 
 # Store the command line argument
-function="$1"
+function=$1
+input_date=$2
 
 # Use the provided value in the path
 for json in downloaded_files/trio-oref-validation/algorithm-comparisons/*/0.3.0/"${function}"/*/*.json; do
-    python scripts/list_errors.py $function < $json
+    path_date=$(echo $json | awk -F'/' '{print $4}')
+    if [[ "$path_date" > "$input_date" ]] || [[ "$path_date" = "$input_date" ]]; then
+	python scripts/list_errors.py $function < $json
+    fi
 done

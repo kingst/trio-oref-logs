7cef70cbd12589660fb52a0ae6c6c75505dac2bb
Sam King <kingst@gmail.com>
2025-07-08 09:42:49 -0700

    Get error counts per day in check_for_errors.sh

--- check_for_errors.sh
+++ check_for_errors.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 # Check if an argument was provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <function> YYYY-MM-DD"
-    echo "Example: $0 iob 2025-03-15"
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 YYYY-MM-DD"
+    echo "Example: $0 2025-03-15"
     exit 1
 fi
 
 # Store the command line argument
-function=$1
-input_date=$2
+input_date=$1
 
-# Use the provided value in the path
-for json in downloaded_files/trio-oref-validation/algorithm-comparisons/${input_date}/0.5.0/"${function}"/*/*.json; do
-    python scripts/list_errors.py $function < $json
+for func in autosens determineBasal iob profile meal; do
+    # Use the provided value in the path
+    echo "Checking ${func}"
+    count=0
+    shopt -s nullglob
+    for json in downloaded_files/trio-oref-validation/algorithm-comparisons/${input_date}/0.5.0/"${func}"/*/*.json; do
+        count=$((count + $(python3 scripts/list_errors.py $func < "$json" | wc -l)))
+    done
+    echo "Found ${count} errors for ${func}"
 done
